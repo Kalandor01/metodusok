e5bd3a9af7882c17160c532f78a02f1cb177fc3a
4K05H <rbenakos@gmail.com>
2022-03-03 15:29:16 +0100

    list

--- src/metodusok/list.java
+++ src/metodusok/list.java
@@ -1,5 +1,22 @@
 package metodusok;
+
+import java.util.ArrayList;
+import java.util.Random;
+
 public class list
 {
+    public static int [] make_lis(int num)
+    {
+        int[] list = new int[num];
+        for (int x = 0; x < num; x++)
+        {
+            list[x] = new Random.nextInt();
+        }
+        return list;
+    }
     
+    public static void main(String[] args)
+    {
+        System.out.println(make_lis(45));
+    }
 }
