8865c37b9be2c9c5a6e8f9bd815e437706ce8ac6
4K05H <rbenakos@gmail.com>
2022-03-03 15:33:59 +0100

    listfix + classes :(

--- src/metodusok/list.java
+++ src/metodusok/list.java
@@ -1,16 +1,15 @@
 package metodusok;
 
 import java.util.ArrayList;
-import java.util.Random;
 
 public class list
 {
-    public static int [] make_lis(int num)
+    public static ArrayList make_lis(int num)
     {
-        int[] list = new int[num];
+        ArrayList list = new ArrayList();
         for (int x = 0; x < num; x++)
         {
-            list[x] = new Random.nextInt();
+            list.add(x);
         }
         return list;
     }
---
+++ src/oop/classes.java
@@ -0,0 +1,22 @@
+package oop;
+class class2
+{    
+    void method2()
+    {
+        
+    }
+}
+
+
+public class classes
+{
+    public static void main(String[] args)
+    {
+        
+    }
+    
+    void method()
+    {
+        
+    }
+}
